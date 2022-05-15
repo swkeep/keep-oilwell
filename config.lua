b2115e617be85c391fe0d2d4ab719e5a2d6d3280
swkeep <badaosoft@gmail.com>
2022-05-15 17:13:27 +0430

    added Delivery

--- config.lua
+++ config.lua
@@ -13,3 +13,25 @@ Config.locations = {
           model = 'v_ind_cm_electricbox',
      },
 }
+
+Config.Delivery = {
+     TriggerLocation = vector3(1475.82, -1855.29, 72.05),
+     SpawnLocation = vector4(1495.04, -1850.61, 71.2, 109.22),
+     DinstanceToTrigger = 10.0,
+     vehicleModel = 'CGT'
+}
+
+Config.Settings = {
+     capacity = {
+          oilbarell = {
+               size = 5000, -- gal
+               price = 500
+          },
+          gasoline = {
+               size = 5000, -- gal
+               price = 500
+          },
+     }
+}
+
+-- vector3(998.08, -1859.08, 30.89)
